5f60363aae809c976c3cdab8aa7d8fc1599a7c90
Tapas Dey <Tapas Dey@DESKTOP-5U9Q831>
2025-08-15 14:54:48 +0530

    ActualFramework

---
+++ ActaulFramework/src/test/java/Organization/ToCreateOrganizationTest.java
@@ -0,0 +1,11 @@
+package Organization;
+
+public class ToCreateOrganizationTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Subrata Organization");
+
+	}
+
+}
