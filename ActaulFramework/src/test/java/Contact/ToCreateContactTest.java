10662c5dc2442a9b215f39a1f6f2d08afb61cf37
Dell <Dell@DESKTOP-VI0LV84>
2025-08-15 17:14:06 +0530

    done

--- ActaulFramework/src/test/java/Contact/ToCreateContactTest.java
+++ ActaulFramework/src/test/java/Contact/ToCreateContactTest.java
@@ -5,7 +5,7 @@ public class ToCreateContactTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Subrata");
-		System.out.println("shreya updated");
+		System.out.println("shreya update");
 
 	}
 
